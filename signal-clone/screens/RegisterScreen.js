54ca31c232239b4fc2e0db25c997b5d24b86f534
Maharsh <gmaharsh@yahoo.com>
2021-01-30 00:48:39 -0600

    Built Register Module

--- signal-clone/screens/RegisterScreen.js
+++ signal-clone/screens/RegisterScreen.js
@@ -1,12 +1,64 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar'
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import { Input } from 'react-native-elements'
 
-const RegisterScreen = ({navigation}) => {
+const RegisterScreen = ({ navigation }) => {
+    
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
+    const register = () => {
+        
+    }
+
     return (
-        <View>
-            <Text>I am Register Component</Text>
-        </View>
+        <KeyboardAvoidingView style={styles.container}>
+            <StatusBar style="light" />
+            {/* <Text h3 style={{ marginBottom: 50 }}>
+                Create a signal account
+            </Text> */}
+
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder="Full Name"
+                    autofocus
+                    type="text"
+                    value={name}
+                    onChangeText={(text) =>setName(text)}
+                />
+                <Input
+                    placeholder="Email"
+                    autofocus
+                    type="text"
+                    value={email}
+                    onChangeText={(text) =>setEmail(text)}
+                />
+                <Input
+                    placeholder="Password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={(text) =>setPassword(text)}
+                    autofocus
+                />
+                <Input
+                    placeholder="ImageUrl(Optional)"
+                    autofocus
+                    type="text"
+                    value={imageUrl}
+                    onChangeText={(text) => setImageUrl(text)}
+                    onSubmit={register}
+                />
+            </View>
+        </KeyboardAvoidingView>
     )
 }
 
 export default RegisterScreen;
+
+const styles = StyleSheet.create({
+    container: {},
+    inputContainer: {}
+})
