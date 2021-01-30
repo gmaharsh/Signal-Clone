421f035ba48d2c93ee6376890c12f284d4b64b35
Maharsh <gmaharsh@yahoo.com>
2021-01-30 12:31:11 -0600

    Added Rooms to database

--- signal-clone/screens/AddChat.js
+++ signal-clone/screens/AddChat.js
@@ -1,12 +1,53 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native';
+import { Icon, Input, Button } from 'react-native-elements';
+import { db } from '../firebase';
 
-const AddChat = ({navigation}) => {
+const AddChat = ({ navigation }) => {
+
+    const [input, setInput] = useState("");
+    
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: 'Add a New Chat',
+            headerBackTitle:"Chats"
+        })
+    }, [navigation])
+    
+    const createChat = async () => {
+        await db.collection("chats").add({
+            chatName: input
+        }).then(() => {
+            navigation.goBack();
+        })
+        .catch(err => {
+            alert(error)
+        })
+    }
+
+
     return (
-        <View>
-            <Text>This is add chat screen</Text>
+        <View style={styles.container}>
+            <Input
+                placeholder="Enter a new chat"
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                onSubmitEditing={createChat}
+                leftIcon={
+                    <Icon name="wechat" type="antdesign" size={24} color="black" />
+                }
+            />
+            <Button onPress={createChat} title="Create New Chat" />
         </View>
     )
 }
 
-export default AddChat
+export default AddChat;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        padding: 30,
+        width:"100%"
+    }
+})
